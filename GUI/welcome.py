2fda445bbfc3d3e822ee403715ad766d5f23efb3
Kenny Dubroff <kennydubroff@hotmail.com>
2023-05-31 20:39:34 -0700

    updated uwsgi and moved api to end

--- GUI/welcome.py
+++ GUI/welcome.py
@@ -10,35 +10,15 @@ app = Flask(__name__)
 
 bot = ChatBot()
 
-api = Api(app, version='1.0', doc='/api-docs', title='Pursuit Prophet API', description='Pursuit Prophet backend')
-
-ns = api.namespace('bot', description='Bot operations')
-
-role_model = api.model('Role', {
-    'role': fields.String(required=True, description="The user's primary goal"),
-})
-
-@ns.route('/role')
-class BotRole(Resource):
-    @ns.expect(role_model, validate=True) 
-    @ns.response(200, 'Role set successfully')
-    @ns.response(400, 'Validation Error')
-    def post(self):
-        '''Set a new role for the bot'''
-        role = request.json.get('role')
-        bot.set_assistant_primary_goal(role)
-
-        return {'role': f'{role}'}, 200
-
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    if request.method == 'POST':        
+    if request.method == 'POST':
+        print('bar')
         user_name = request.form.get('user_name')
         if user_name:  # checks if the field isn't empty
             return redirect(url_for('role_selection'))
-        else:
-            print('posted!')
     else:
+        print('foo')
         name = "Pursuit Prophet"
         return render_template('welcome.html', app_name=name)
 
@@ -52,6 +32,26 @@ def role_selection():
         roles = ['Write a blog post about cats', 'Organize my house', 'Learn about quantum physics']
         return render_template('role_selection.html', roles=roles)
 
+#region API    
+api = Api(app, version='1.0', doc='/api-docs', title='Pursuit Prophet API', description='Pursuit Prophet backend')
+
+role_model = api.model('Role', {
+    'role': fields.String(required=True, description="The user's primary goal"),
+})
+
+ns = api.namespace('api', description='Bot API')
+@ns.route('/role')
+class BotRole(Resource):
+    @ns.expect(role_model, validate=True) 
+    @ns.response(200, 'Role set successfully')
+    @ns.response(400, 'Validation Error')
+    def post(self):
+        '''Set a new role for the bot'''
+        role = request.json.get('role')
+        bot.set_assistant_primary_goal(role)
+
+        return {'role': f'{role}'}, 200
+#endregion API
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     if port is None:
