41fbb9dad56fc50596b112356e0d7e6d86512e39
Kenny Dubroff <kennydubroff@hotmail.com>
2023-05-31 22:30:38 -0700

    Merged restx

--- GUI/welcome.py
+++ GUI/welcome.py
@@ -1,9 +1,10 @@
+import os
 import sys
 from flask import Flask, render_template, request, redirect, url_for
+from flask_restx import Api, Resource, fields
 
 sys.path.append("..")
 from bot import ChatBot
-
 
 app = Flask(__name__)
 
@@ -11,12 +12,14 @@ bot = ChatBot()
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    if request.method == 'POST':        
+    if request.method == 'POST':
+        print('bar')
         user_name = request.form.get('user_name')
         if user_name:  # checks if the field isn't empty
             return redirect(url_for('role_selection'))
     else:
-        name = "PursuitProphet"
+        print('foo')
+        name = "Pursuit Prophet"
         return render_template('welcome.html', app_name=name)
 
 @app.route('/role_selection', methods=['GET', 'POST'])
@@ -29,5 +32,28 @@ def role_selection():
         roles = ['Write a blog post about cats', 'Organize my house', 'Learn about quantum physics']
         return render_template('role_selection.html', roles=roles)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#region API    
+api = Api(app, version='1.0', doc='/api-docs', title='Pursuit Prophet API', description='Pursuit Prophet backend')
+
+role_model = api.model('Role', {
+    'role': fields.String(required=True, description="The user's primary goal"),
+})
+
+ns = api.namespace('api', description='Bot API')
+@ns.route('/role')
+class BotRole(Resource):
+    @ns.expect(role_model, validate=True) 
+    @ns.response(200, 'Role set successfully')
+    @ns.response(400, 'Validation Error')
+    def post(self):
+        '''Set a new role for the bot'''
+        role = request.json.get('role')
+        bot.set_assistant_primary_goal(role)
+
+        return {'role': f'{role}'}, 200
+#endregion API
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    if port is None:
+        port = 5000
+    app.run(host='0.0.0.0', port=port)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uwsgi --ini GUI/uwsgi.ini
