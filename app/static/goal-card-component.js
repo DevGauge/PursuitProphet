10ccd71939eafdadf7f7146f9505dd8b722e2861
Kenny Dubroff <kennydubroff@hotmail.com>
2023-08-20 17:55:25 -0700

    can complete tasks

--- app/static/goal-card-component.js
+++ app/static/goal-card-component.js
@@ -1,13 +1,13 @@
 window.onload = function () {
     var goalCards = document.querySelectorAll('.goal-card');
     var isTaskPage = window.location.href.includes('task');
-    var isSubTaskPage = window.location.href.includes('subtask');
-    console.log('This is a task page: ', isTaskPage)
+    var isSubTaskPage = window.location.href.includes('subtask');    
     goalCards.forEach(card => {
         // Stop propagation for buttons
         var chatButton = card.querySelector('.chat-button');
         var deleteButton = card.querySelector('.delete-button');
-        [chatButton, deleteButton].forEach(btn => {
+        var completeButton = card.querySelector('.checkbox-container');
+        [chatButton, deleteButton, completeButton].forEach(btn => {
             btn.addEventListener('click', e => e.stopPropagation());
         });
 
@@ -30,6 +30,18 @@ window.onload = function () {
                 showAlert('warning', `Are you sure you want to delete the subtask "${goalTitle}"?`, `/delete_subtask/${goalId}`, 'Delete');
             }            
         });
+
+        completeButton.addEventListener('click', e => {
+            const clickedCard = e.currentTarget.parentElement;            
+            const goalId = clickedCard.getAttribute('data-task-id');
+            const goalTitle = clickedCard.querySelector('.goal-text').innerText;
+            if (!isTaskPage && !isSubTaskPage) { // is goal
+                window.location.href = `/goal/complete/${goalId}`;
+            } else {
+                window.location.href = `/task/complete/${goalId}`;
+            }
+        });
+
 
         // Handle click on rest of card
         card.addEventListener('click', e => {
