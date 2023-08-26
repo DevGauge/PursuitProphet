c76ad362bf6e78ce9b7af0d9e48e0bc6b60edd04
Kenny Dubroff <kennydubroff@hotmail.com>
2023-08-25 20:33:07 -0700

    added app context

--- app/user_tutorial_columns_migration.py
+++ app/user_tutorial_columns_migration.py
@@ -1,33 +1,36 @@
 from app.app import db, User
+from flask import current_app
+
 
 def migrate():
-    # Query all existing rows
-    print('Migrating user tutorial columns...')
-    rows_to_update = User.query.all()
+    with current_app.app_context():
+        # Query all existing rows
+        print('Migrating user tutorial columns...')
+        rows_to_update = User.query.all()
 
-    columns = [
-        'is_first_login',
-        'is_first_detail_view',
-        'is_first_new_goal',
-        'is_first_new_task',
-        'is_first_new_subtask',
-        'is_first_demo_task_gen',
-        'is_first_demo_subtask_gen'
-    ]
+        columns = [
+            'is_first_login',
+            'is_first_detail_view',
+            'is_first_new_goal',
+            'is_first_new_task',
+            'is_first_new_subtask',
+            'is_first_demo_task_gen',
+            'is_first_demo_subtask_gen'
+        ]
 
-    # Update each row with the default values
-    for row in rows_to_update:
-        for column in columns:
-            if getattr(row, column) is None:
-                setattr(row, column, True)
-        if getattr(row, 'is_demo_finished') is None:
-            setattr(row, 'is_demo_finished', False)
+        # Update each row with the default values
+        for row in rows_to_update:
+            for column in columns:
+                if getattr(row, column) is None:
+                    setattr(row, column, True)
+            if getattr(row, 'is_demo_finished') is None:
+                setattr(row, 'is_demo_finished', False)
 
-    print('Committing changes...')
+        print('Committing changes...')
 
-    # Commit the changes
-    db.session.commit()
-    print('Migration successful')
+        # Commit the changes
+        db.session.commit()
+        print('Migration successful')
 
 def reset():
     print('Resetting user tutorial columns...')
