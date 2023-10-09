ce8fa41198044bbedeee0e9e16bafe81a4fdfed6
Kenny Dubroff <kennydubroff@hotmail.com>
2023-10-08 19:19:34 -0700

    reorganized task routes

--- features/task/task_blueprint.py
+++ features/task/task_blueprint.py
@@ -1,11 +1,16 @@
 from app.models import Task, Goal
 from app.pp_logging.db_logger import db
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, DateField, TimeField
 from wtforms.validators import DataRequired, Optional
 from shared.blueprints.blueprints import task_bp 
+from app.models import ChatBot
+from bot import GoalManager, IOManager
+
+goal_generator_bot = GoalManager(io_manager = IOManager())
+bot = ChatBot(goal_generator_bot, None)
 
 class TaskForm(FlaskForm):
     goal = StringField('Task Name', validators=[DataRequired()], render_kw={"placeholder": "What do you need to do?"})
@@ -32,7 +37,7 @@ def new_task(goal_id: str):
         db.session.commit()
         flash('Your task has been added.', 'success')
         return redirect(url_for('view_tasks', goal_id=goal_id, title=goal.goal))
-    return render_template('dream_db.dream-detail.html', form=form, goal=goal)
+    return render_template('task-detail.html', form=form, task=None, goal=goal)
 
 @task_bp.route('/task/<task_id>', methods=['GET', 'POST'])
 def task_detail(task_id):
@@ -45,7 +50,7 @@ def task_detail(task_id):
         task.target_time = form.target_time.data
         db.session.commit()
         flash('Your task has been updated.', 'success')
-        return render_template('task-detail.html', form=form, goal=task)
+        return render_template('task-detail.html', form=form, task=task)
         
     else:
         if task is None:
@@ -55,5 +60,11 @@ def task_detail(task_id):
             print(task.task)
             form = TaskForm(obj=task)
             form.submit.label.text = 'Update Task'
-            return render_template('task-detail.html', form=form, goal=task)
-        
+            return render_template('task-detail.html', form=form, task=task)
+
+@task_bp.route('/task/generate_subtasks/<int:num_subtasks>', methods=['GET', 'POST'])
+def generate_subtasks(num_subtasks):
+    task_id = request.args.get('task_id')
+    task = Task.query.filter_by(id=task_id).first()
+    bot.generate_subtasks(task, num_subtasks)
+    return redirect(url_for('view_subtasks', task_id=task.id))
