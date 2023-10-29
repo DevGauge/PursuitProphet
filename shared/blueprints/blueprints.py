aa4af14d37072f5cb272f468019e15292c254717
Kenny Dubroff <kennydubroff@hotmail.com>
2023-10-29 09:50:53 -0700

    added google blueprint

--- shared/blueprints/blueprints.py
+++ shared/blueprints/blueprints.py
@@ -1,8 +1,18 @@
 import os
 from flask import Blueprint
+from flask_dance.contrib.google import google, make_google_blueprint
+from google.oauth2 import id_token
+from app.models import User
+from app.pp_logging.db_logger import db
+from flask import flash, request
+from flask_login import login_user
+from flask import redirect, url_for, session
+
+from dotenv import load_dotenv
+load_dotenv()
 
 def construct_blueprint(name: str):
-    """Constructs a blueprint with the given name and template_folder, which defaults to the name"""
+    """Constructs a blueprint with the given name. The blueprint's template and static folders are located in the features/<name>/template or /static folder."""
     current_dir = os.path.dirname(os.path.abspath(__file__))
     root_path = os.path.abspath(os.path.join(current_dir, '../../features'))
     template_path = os.path.join(root_path, name, 'templates')
@@ -15,8 +25,68 @@ dream_bp = construct_blueprint('dream')
 task_bp = construct_blueprint('task')
 subtask_bp = construct_blueprint('subtask')
 
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+
+google_bp = make_google_blueprint(
+    client_id=GOOGLE_CLIENT_ID,
+    client_secret=GOOGLE_CLIENT_SECRET,
+    scope=["profile", "email"]
+)
+
 def register_blueprints(flask_app):
     blueprints = [demo_bp, dream_bp, task_bp, subtask_bp]
     for blueprint in blueprints:
         flask_app.register_blueprint(blueprint)
         print(f'registered blueprint: {blueprint.name} with url_prefix: {blueprint.url_prefix}, template_folder: {blueprint.template_folder}, static_folder: {blueprint.static_folder}')
+        
+    flask_app.register_blueprint(google_bp, url_prefix='/google_login')
+    
+
+def get_user_info(): 
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    resp = google.get("https://people.googleapis.com/v1/people/me?key=" + GOOGLE_API_KEY + "&personFields=names,emailAddresses")
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    return resp
+
+def login_user_with_google():
+    """Logs the user in using their Google account."""
+
+    user_info = get_user_info()
+    email = user_info["email"]
+
+    user = User.query.filter_by(email=email).first()
+    if user is not None:
+        login_user(user)
+    else:
+        # Create a new user account.
+        user = User(email=email)
+        db.session.add(user)
+        db.session.commit()
+
+        # Log the user in.
+        login_user(user)
+
+    return redirect(url_for('dashboard'))
+
+@google_bp.route("/login", endpoint='google_bp_login')
+def login():
+    GOOGLE_CALLBACK_URI = os.getenv('GOOGLE_CALLBACK_URI')
+    # Redirect the user to the Google OAuth login page.
+    return google.authorize(callback_uri=GOOGLE_CALLBACK_URI)
+
+@google_bp.route('/callback', methods=['GET', 'POST'])
+def callback():
+    oauth_resp = get_user_info()
+
+    # Log the user in using their Google account.
+    login_user_with_google()
+
+    flash("Successfully authenticated.", category="success")
+    return redirect(url_for('dashboard'))
+
+@google_bp.route("/logout", endpoint='google_bp_logout')
+def logout():
+    session.clear()
+    return redirect(url_for("dashboard"))
