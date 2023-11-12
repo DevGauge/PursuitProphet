2b891c8518ee7cfe67af41fb1ffabdacae247a97
Kenny Dubroff <kennydubroff@hotmail.com>
2023-11-12 12:45:54 -0800

    removed google routes from shared blueprint space

--- shared/blueprints/blueprints.py
+++ shared/blueprints/blueprints.py
@@ -1,42 +1,6 @@
 import os
-from flask import Blueprint, url_for
-
-# Google Oauth
-from authlib.integrations.flask_client import OAuth
-# TODO use blueprint instead of app
+from flask import Blueprint
 from app.app import app
-from dotenv import load_dotenv
-load_dotenv()
-
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=os.getenv('GOOGLE_CLIENT_ID'),
-    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    client_kwargs={'scope': 'email profile'},
-)
-
-@app.route('/login/google')
-def login_google():
-    redirect_uri = url_for('authorize', _external=True)
-    print(f"redirecting to {redirect_uri}")
-    return google.authorize_redirect(redirect_uri)
-
-@app.route('/authorize')
-def authorize():
-    token = google.authorize_access_token()
-    resp = google.get('userinfo')
-    if resp.ok:
-        user_info = resp.json()
-        print(user_info)
-        email = user_info['email']
-        return f'You are {email} on Google'
-    return 'Bad Resp'
 
 def construct_blueprint(name: str):
     """Constructs a blueprint with the given name and template_folder, which defaults to the name"""
@@ -51,9 +15,10 @@ demo_bp = construct_blueprint('demo')
 dream_bp = construct_blueprint('dream')
 task_bp = construct_blueprint('task')
 subtask_bp = construct_blueprint('subtask')
+google_bp = construct_blueprint('google')
 
 def register_blueprints(flask_app):
-    blueprints = [demo_bp, dream_bp, task_bp, subtask_bp]
+    blueprints = [demo_bp, dream_bp, task_bp, subtask_bp, google_bp]
     for blueprint in blueprints:
         flask_app.register_blueprint(blueprint)
         print(f'registered blueprint: {blueprint.name} with url_prefix: {blueprint.url_prefix}, template_folder: {blueprint.template_folder}, static_folder: {blueprint.static_folder}')
