1a23984c29bdfcf50555dd76fa5835dc40264d86
Kenny Dubroff <kennydubroff@hotmail.com>
2023-11-12 11:08:16 -0800

    initial implementation - get user info

--- shared/blueprints/blueprints.py
+++ shared/blueprints/blueprints.py
@@ -1,5 +1,42 @@
 import os
-from flask import Blueprint
+from flask import Blueprint, url_for
+
+# Google Oauth
+from authlib.integrations.flask_client import OAuth
+# TODO use blueprint instead of app
+from app.app import app
+from dotenv import load_dotenv
+load_dotenv()
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv('GOOGLE_CLIENT_ID'),
+    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'email profile'},
+)
+
+@app.route('/login/google')
+def login_google():
+    redirect_uri = url_for('authorize', _external=True)
+    print(f"redirecting to {redirect_uri}")
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    if resp.ok:
+        user_info = resp.json()
+        print(user_info)
+        email = user_info['email']
+        return f'You are {email} on Google'
+    return 'Bad Resp'
 
 def construct_blueprint(name: str):
     """Constructs a blueprint with the given name and template_folder, which defaults to the name"""
